ca2518bab47f6deb43dc8a128a97ddcc5a876f1a
Giordano Cardillo <giordanocardillo@users.noreply.github.com>
2017-11-30 17:15:01 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,41 +1,9 @@
 #!/bin/sh
+DEPS_PACKAGES="libnice10 libglib2.0-0 libjansson4 libssl1.0.0 libcurl3 libsofia-sip-ua0 libogg0 libopus0 libmicrohttpd10 git wget curl"
+BUILD_PACKAGES="build-essential libmicrohttpd-dev libjansson-dev libnice-dev libssl-dev libsrtp-dev libsofia-sip-ua-dev libglib2.0-dev libopus-dev libogg-dev libcurl4-openssl-dev libavutil-dev libavcodec-dev libavformat-dev pkg-config gengetopt libtool automake cmake"
 set -e
 cd /root
-apt-get update && apt-get install -y \
-  libnice10 \
-  libglib2.0-0 \
-  libjansson4 \
-  libssl1.0.0 \
-  libcurl3 \
-  libsofia-sip-ua0 \
-  libogg0 \
-  libopus0 \
-  libmicrohttpd10 \
-  libmicrohttpd-dev \
-  libjansson-dev \
-  libnice-dev  \
-  libssl-dev \
-  libsrtp-dev \
-  libsofia-sip-ua-dev \
-  libglib2.0-dev \
-  libopus-dev \
-  libogg-dev \
-  libcurl4-openssl-dev \
-  libavutil-dev \
-  libavcodec-dev \
-  libavformat-dev \
-  libavutil-ffmpeg54 \
-  libavcodec-ffmpeg56 \
-  libavformat-ffmpeg56 \
-  pkg-config \
-  gengetopt \
-  libtool \
-  automake \
-  cmake \
-  git \
-  sudo \
-  wget \
-&& rm -rf /var/lib/apt/lists/*
+apt-get update && apt-get install -y $DEPS_PACKAGES $BUILD_PACKAGES && rm -rf /var/lib/apt/lists/*
 wget https://github.com/cisco/libsrtp/archive/v2.0.0.tar.gz
 tar xfv v2.0.0.tar.gz && rm -rf v2.0.0.tar.gz
 cd libsrtp-2.0.0
@@ -67,26 +35,7 @@ make install
 make configs
 cd /root
 rm -rf janus-gateway
-apt-get autoremove -y \
-  libmicrohttpd-dev \
-  libjansson-dev \
-  libnice-dev  \
-  libssl-dev \
-  libsrtp-dev \
-  libsofia-sip-ua-dev \
-  libglib2.0-dev \
-  libopus-dev \
-  libogg-dev \
-  libcurl4-openssl-dev \
-  libavutil-dev \
-  libavcodec-dev \
-  libavformat-dev \
-  gengetopt \
-  libtool \
-  automake \
-  cmake \
-  git \
-  wget
+apt-get autoremove -y $BUILD_PACKAGES
 cp -R /opt/janus/etc/janus /root/janus
 apt-get clean
 rm -rf build.sh
